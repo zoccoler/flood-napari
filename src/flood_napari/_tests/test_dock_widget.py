b71b81245d2c56d33383031cb3f4f9d54a667947
Marcelo Zoccoler <marcelozoc@gmail.com>
2021-12-08 18:21:51 +0100

    Update test_dock_widget.py

--- src/flood_napari/_tests/test_dock_widget.py
+++ src/flood_napari/_tests/test_dock_widget.py
@@ -4,7 +4,7 @@ import pytest
 # this is your plugin name declared in your napari.plugins entry point
 MY_PLUGIN_NAME = "flood-napari"
 # the name of your widget(s)
-MY_WIDGET_NAMES = ["Example Q Widget", "example_magic_widget"]
+MY_WIDGET_NAMES = ["Qt_Designer_flood", "flood_magic_factory", "FunctionGui_flood"]
 
 
 @pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
