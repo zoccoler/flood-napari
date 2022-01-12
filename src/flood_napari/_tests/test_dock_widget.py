ae1aee3c86431a5e257b318f19a721b63becef51
Marcelo Zoccoler <marcelozoc@gmail.com>
2022-01-12 15:16:21 +0100

    Update test_dock_widget.py

--- src/flood_napari/_tests/test_dock_widget.py
+++ src/flood_napari/_tests/test_dock_widget.py
@@ -4,12 +4,12 @@ import pytest
 # this is your plugin name declared in your napari.plugins entry point
 MY_PLUGIN_NAME = "flood-napari"
 # the name of your widget(s)
-MY_WIDGET_NAMES = ["Qt_Designer_flood", "flood_magic_factory", "FunctionGui_flood"]
+MY_WIDGET_NAMES = ["Qt Designer flood", "flood_magic_factory", "FunctionGui flood"]
 
 
 @pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
 def test_something_with_viewer(widget_name, make_napari_viewer, napari_plugin_manager):
-    # napari_plugin_manager.register(flood_napari, name=MY_PLUGIN_NAME)
+    napari_plugin_manager.register(flood_napari, name=MY_PLUGIN_NAME)
     viewer = make_napari_viewer()
     num_dw = len(viewer.window._dock_widgets)
     viewer.window.add_plugin_dock_widget(
